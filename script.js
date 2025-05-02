85b67d6e8f07b29a81fd33d9c6015c29fce49d47
emiliano-chp-dev <echp.dev@gmail.com>
2025-05-02 20:42:42 +0300

    Store DOM elements inside constants

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 "use strict";
 
 function main() {
+    // Store DOM elements
+    const controlPanelElement = document.querySelector('.control_panel');
+    const boardElement = document.querySelector('.board');
+
     // ...
 }
 
