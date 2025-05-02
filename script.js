b0637af487d3a73b6d13657fdac50447a5c368b9
emiliano-chp-dev <echp.dev@gmail.com>
2025-05-02 20:45:31 +0300

    Create function to set initial state of the drawing board

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ function main() {
     const controlPanelElement = document.querySelector('.control_panel');
     const boardElement = document.querySelector('.board');
 
+    // Core functions
+    setBoard();
+
+    // Sets the default board
+    function setBoard(num=16) {
+        let count = num;
+        resetBoard();
+        createTiles(count);
+        appendTiles();
+    }
+
     // ...
 }
 
