a4ceefb90d8d9a35f4fc42bbc3ae37fdc544312a
emiliano-chp-dev <echp.dev@gmail.com>
2025-05-03 11:57:36 +0300

    Create logic to add a 16x16 grid of tiles to the board

--- script.js
+++ script.js
@@ -10,16 +10,39 @@ function main() {
 
     // Sets the default board
     function setBoard(num=16) {
-        let count = num;
         resetBoard();
-        createTiles(count);
-        appendTiles();
+        const tiles = createTilesGroup(num);
+        appendTiles(tiles);
     }
 
     // Empties the board
     function resetBoard() {
         boardElement.innerHTML = '';
     }
+
+    // Creates a single tile
+    function createTileElement(count) {
+        const tileElement = document.createElement('div');
+        tileElement.classList.add('tile');
+        tileElement.style.width = `${boardElement.clientWidth/count}px`;
+        tileElement.style.height = `${boardElement.clientWidth/count}px`;
+        return tileElement;
+    }
+
+    // Create tiles group
+    function createTilesGroup(count) {
+        const tilesGroup = [];
+        for (let i = 1; i <= count * count; i++) {
+            tilesGroup.push(createTileElement(count));
+        }
+        return tilesGroup;
+    }
+
+    // Append tiles
+    function appendTiles(tiles) {
+        tiles.forEach(tile => boardElement.appendChild(tile));
+    }
+    
 
     // ...
 }
